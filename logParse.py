519085109022a8ac998522a5e0328c98948ed307
jayv <jayv@jfrog.com>
2021-04-27 14:41:31 -0400

    test commit

---
+++ logParse.py
@@ -0,0 +1,22 @@
+import textparser
+from textparser import Sequence
+
+
+class Parser(textparser.Parser):
+
+    def token_specs(self):
+        return [
+            ('SKIP',          r'[ \r\n\t]+'),
+            ('WORD',          r'\w+'),
+            ('EMARK',    '!', r'!'),
+            ('COMMA',    ',', r','),
+            ('MISMATCH',      r'.')
+        ]
+
+    def grammar(self):
+        return Sequence('WORD', ',', 'WORD', '!')
+
+
+tree = Parser().parse('Hello, World!')
+
+print('Tree:', tree)
