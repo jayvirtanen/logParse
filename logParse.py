ae43d79b46453602fe68ce1f99e95b0e500ff5ed
jayv <jayv@jfrog.com>
2021-04-27 16:08:44 -0400

    made it pretty

--- logParse.py
+++ logParse.py
@@ -1,21 +1,44 @@
 from collections import Counter
 import sys
+
+#String array for printing end result
 total = []
+#String array to check against to make sure entries aren't duplicated in 'total'
 checker = []
+#String array for parsing the file for raw error messages
+error_list = []
+
+#Accept input file
+
 with open(sys.argv[1], 'r') as f:
-	content_list = []
+
 	for line in f:
+
 		if "ERROR" in line:
-			line.rstrip()
+
 			segments = line.split('] - ')
+
+#Splits the line at the delimiter between the "extra information" and the actual error message
+
 			line = segments[1]
-			line = line.replace('\n', '') 
-			content_list.append(line)
-count = Counter(content_list)
+			line = line.replace('\n', '')
+
+#Removes superfluous newline character at the end of the line before being added to error list
+
+			error_list.append(line)
+
+#Counter creates a paired list of each error and the number of times it has occurred
+
+count = Counter(error_list)
 for i in count.elements():
+
+#unpairs i (error message) and count[i] (occurrence number) by only passing each unique message to be appended to 'total' once
+
 	if i not in checker:
+
 		checker.append(i)
-		total.append("ERROR: " + i +"::: appears in logs " + str(count[i]) + " times")
+		total.append("ERROR: " + i[0:100] +" OCCURENCES: " + str(count[i]))
+
 for i in total:
 	print(i)
 
